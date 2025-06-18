d61308bdf07da5e17f34f09286301822731ff38e
Pedro Henrique Custodio <custodiou07@gmail.com>
2025-06-18 08:55:11 -0300

    first commit

---
+++ src/test/java/com/cuztodio/CadastroCRUD/CadastroCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuztodio.CadastroCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
