f8c2d55c5fc6cfed6c875073b26fef7d1dc1af66
Pedro Henrique Custodio <custodiou07@gmail.com>
2025-06-19 17:46:17 -0300

    feat: criacao repository da classe tarefa

---
+++ src/main/java/com/cuztodio/CadastroCRUD/repository/TarefasRepository.java
@@ -0,0 +1,8 @@
+package com.cuztodio.CadastroCRUD.repository;
+
+import com.cuztodio.CadastroCRUD.model.TarefaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TarefasRepository extends JpaRepository<TarefaModel, Long>{
+
+}
