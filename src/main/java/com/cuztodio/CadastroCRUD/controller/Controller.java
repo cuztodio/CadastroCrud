85b71e4abd6040f5945a1fff84137d17a322fbc8
Pedro Henrique Custodio <custodiou07@gmail.com>
2025-06-24 20:14:08 -0300

    feat: adiciona as requisiçoe http no controller da classe Pessoa

--- src/main/java/com/cuztodio/CadastroCRUD/controller/Controller.java
+++ src/main/java/com/cuztodio/CadastroCRUD/controller/Controller.java
@@ -1,15 +1,32 @@
 package com.cuztodio.CadastroCRUD.controller;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/pessoa")
 public class Controller {
 
-    @GetMapping("/boasVindas")
-    public String boasVindas(){
-        return "Ola Usuario!";
+    @PostMapping("/criar")
+    public String criaPessoa(){
+        return "Pessoa cadastrada!";
+    }
+
+    @GetMapping("/buscar/{id}")
+    public String exibiPessoa(){
+        return "Pessoa mostrada!";
+    }
+
+    @GetMapping("/buscar/todos")
+    public String exibiPessoas(){
+        return "Pessoas mostradas";
+    }
+
+    @PutMapping("/alterar/{id}")
+    public String alterarPessoa(){
+        return "Pessoa alterada";
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public String deletarPessoa(){
+        return "Pessoa deletada";
     }
 }
